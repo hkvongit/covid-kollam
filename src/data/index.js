005eea33877c5ab4f5f23c2ee346c38e6a571ed2
Rumi <ruminajeeb@gmail.com>
2020-08-08 19:19:50 +0530

    pdf file edit

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 import Data from './2020-08-08-18-06.json'
 export default Data
 
-export const downloadLink = "https://drive.google.com/file/d/1OJGYBp_HEcfkO4fLOBTG3bLPweYddHNS/view?usp=sharing"
+export const downloadLink = "https://drive.google.com/file/d/1TO96TgMNkQqAo8sIIf0MsVypNBR8AS3x/view?usp=sharing"
 // code editor
