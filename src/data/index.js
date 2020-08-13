597065013455040e6ed9d821b6afbae10989efa1
Rumi <ruminajeeb@gmail.com>
2020-08-13 19:23:10 +0530

    august13

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
-import Data from './2020-08-12-18-09.json'
+import Data from './2020-08-13-18-06.json'
 export default Data
 
-export const downloadLink = "https://drive.google.com/file/d/13RTzYvptm05V9ZbuM4aDxoRyQrKCLDSz/view?usp=sharing"
+export const downloadLink = "https://drive.google.com/file/d/1-izOpx6s2yTTwNo7U0qAW0fUbggIOTLQ/view?usp=sharing"
 // code editor
