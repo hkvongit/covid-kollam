dae672a67f6c55950c58a4c1b3ba21c511b3054d
hari <hari@git.com>
2020-08-17 02:14:56 +0530

    Moved data o thrash

--- src/data/index.js
+++
@@ -1,5 +0,0 @@
-import Data from './2020-08-16-18-06.json'
-export default Data
-
-export const downloadLink = "https://drive.google.com/file/d/1Imwf8Pzi3bqOolSWkFi72-5Zmwqqg54o/view?usp=sharing"
-// code editor
